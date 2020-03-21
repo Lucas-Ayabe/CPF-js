8282d8e60bb998d4818b484a9b10ea8f75cdcf8a
Lucas-Ayabe <lucas.ayabe@gmail.com>
2020-03-21 16:52:21 -0300

    Funcção inicial de validar CPF

---
+++ validateCPF.js
@@ -0,0 +1,55 @@
+function initCPFValidation(target, options) {
+    const cpfInputs = document.querySelectorAll(target);
+
+    cpfInputs.forEach(input => {
+        input.addEventListener("input", e => {
+            validateCPF(ArrayFromCPF(e.target.value), options);
+        });
+    });
+
+    function ArrayFromCPF(cpf) {
+        return cpf.split(".").join("").split("-").join("").split("");
+    }
+
+    function firstCPFDigitValidation(cpf) {
+        return (((cpf[0] * 10) + (cpf[1] * 9) + (cpf[2] * 8) + (cpf[3] * 7) + (cpf[4] * 6) + (cpf[5] * 5) + (cpf[6] * 4) + (cpf[7] * 3) + (cpf[8] * 2)) * 10 % 11) === +cpf[9];
+    }
+
+    function secondCPFDigitValidation(cpf) {
+        return (((cpf[0] * 11) + (cpf[1] * 10) + (cpf[2] * 9) + (cpf[3] * 8) + (cpf[4] * 7) + (cpf[5] * 6) + (cpf[6] * 5) + (cpf[7] * 4) + (cpf[8] * 3) + (cpf[9] * 2)) * 10 % 11) === +cpf[10];
+    }
+
+    function noCPFRepeatedDigitsValidation(cpf) {
+        return !(cpf.every(digit => digit === cpf[0]));
+    }
+
+    function validateCPF(cpf, options) {
+        if (cpf.length === 11) {
+            if (noCPFRepeatedDigitsValidation(cpf)) {
+                if (firstCPFDigitValidation(cpf) && secondCPFDigitValidation(cpf)) {
+                    options.success();
+                } else {
+                    options.error();
+                }
+            } else {
+                options.error();
+            }
+        } else {
+            options.lengthError();
+        }
+    }
+}
+
+const cpfValidationOptions = {
+    success() {
+        console.log("CPF válido");
+    },
+    error() {
+        console.error("CPF inválido");
+    },
+    lengthError() {
+        console.error("CPF deve ter 11 digitos");
+    }
+}
+
+initCPFValidation("input.is-cpf", cpfValidationOptions);
